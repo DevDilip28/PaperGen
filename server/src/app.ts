de1936b3bf373dad8b271a24f75fd3eb32b96295
DevDilip28 <dilipasdeo028@gmail.com>
2026-05-25 00:52:57 +0530

    build command

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,7 +5,15 @@ import assignmentRoutes from "./routes/assignment.route.js"
 
 const app = express();
 
-app.use(cors());
+app.use(
+    cors({
+        origin: [
+            "http://localhost:3000",
+        ],
+        credentials: true,
+    })
+);
+
 app.use(express.json());
 
 app.use("/api/assignments", assignmentRoutes);
