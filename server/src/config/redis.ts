f8c17455c51100d1be536a182fa0cc044de14a8b
DevDilip28 <dilipasdeo028@gmail.com>
2026-05-25 00:46:17 +0530

    redis url update

--- server/src/config/redis.ts
+++ server/src/config/redis.ts
@@ -1,8 +1,6 @@
 import Redis from "ioredis";
 
-const redisConnection = new Redis({
-    host: process.env.REDIS_HOST,
-    port: Number(process.env.REDIS_PORT),
+const redisConnection = new Redis(process.env.REDIS_URL!, {
     maxRetriesPerRequest: null,
 });
 
